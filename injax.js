ceb8675cec4fabe5af0cb04b1e02b9929bc17408
Christian Grauer <mail@cgrauer.de>
2015-06-12 17:05:14 +0200

    integrate css styles

--- injax.js
+++ injax.js
@@ -1,9 +1,9 @@
-
-
 $(document).ready( function() {
-
-    var namespace = 'document';
+    alert("HOLLEBasdasdOLLE")
+    var namespace = 'body';
+    $(namespace).find('.injax-edit .injax-save .injax-close');
     $(namespace).on('click', '.injax-open', function () {
+        alert("GUllideckel");
         $(this).closest('.injax-item').find('.injax-view').hide();
         $(this).closest('.injax-item').find('.injax-open').hide();
         $(this).closest('.injax-item').find('.injax-edit').show();
