6b819b119d1e04a693cfa1530aa8b6777d3c1f41
Christian Grauer <mail@cgrauer.de>
2015-06-12 16:18:33 +0200

    hide and show elements

--- injax.js
+++ injax.js
@@ -2,4 +2,13 @@
 
 $(document).ready( function() {
 
+    var namespace = 'document';
+    $(namespace).on('click', '.injax-open', function () {
+        $(this).closest('.injax-item').find('.injax-view').hide();
+        $(this).closest('.injax-item').find('.injax-open').hide();
+        $(this).closest('.injax-item').find('.injax-edit').show();
+        $(this).closest('.injax-item').find('.injax-close').show();
+        $(this).closest('.injax-item').find('.injax-save').show();
+    });
+
 });
